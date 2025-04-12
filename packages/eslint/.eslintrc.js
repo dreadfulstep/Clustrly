d4ba52cfba00ccaa419283aa0d287700d0ef911a
Dreadfulstep <110997762+dreadfulstep@users.noreply.github.com>
2025-04-12 04:14:51 +0100

    Added and made eslint package, working on env package

---
+++ packages/eslint/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+    'import/no-unresolved': 'error',
+    'node/no-missing-import': 'warn',
+  },
+};
